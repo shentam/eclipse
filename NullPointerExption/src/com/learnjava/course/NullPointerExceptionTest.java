578b4b9e3becaeabfdcf95d104920ae0dddbd8a5
Andrew Chao <andrew.m.chao@gmail.com>
2018-03-15 12:56:46 +0000

    string exception example

---
+++ NullPointerExption/src/com/learnjava/course/NullPointerExceptionTest.java
@@ -0,0 +1,15 @@
+package com.learnjava.course;
+
+public class NullPointerExceptionTest {
+
+	public static void main(String[] args) {
+		String s =null;
+		//将字符串设置为null
+		
+		System.out.println(s.toUpperCase());
+		//尝试将字符串转换为大写，看看回发生什么
+		
+
+	}
+
+}
