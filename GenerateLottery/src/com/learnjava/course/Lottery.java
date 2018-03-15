f33c2c303364f1e5b7ce5354a7e9c23e5581532c
Andrew Chao <andrew.m.chao@gmail.com>
2018-03-15 10:03:43 +0000

    luck number from the 1-59 +1 bonus number from left

--- GenerateLottery/src/com/learnjava/course/Lottery.java
+++ GenerateLottery/src/com/learnjava/course/Lottery.java
@@ -45,52 +45,7 @@ public class Lottery {
 				
 	}
 	
-	public List<String> getRearNumber(){
-		List <String> list = new ArrayList<String>();
-		//创建后半段号码集合，也就是最后2个球的数字
-		
-		String lotteryNumber="";
-		List<String> startList = getHeadNumber();
-		
-		for(int i=1; i<60;i++) {
-			//初始化后半段号码集合
 	
-			if(i<10) {
-				list.add("0"+i+" ");
-				//添加0~9的号码，原理同前半段
-			}
-			else {
-				list.add(""+i+" ");
-				//添加大于9的号码
-			}
-		}
-		
-
-		int roundIndex =0;
-		//创建并初始化随机数
-		
-		List<String> lotteryList= new ArrayList<String>();
-		//保存后半段号码的List集合
-		
-		
-		/*Random r =new Random();
-		roundIndex=r.nextInt(list.size());
-		lotteryNumber=list.get(roundIndex);
-		lotteryList.add(lotteryNumber);
-		*/
-		for(int j=0; j<2; j++) {
-			int amount= list.size();//获取后半段号码的个数
-			Random r = new Random();//创建并实例化Random的对象
-			roundIndex=r.nextInt(amount);//获取一个0到amount-1的随机数
-			lotteryNumber=list.get(roundIndex);//摇号
-			lotteryList.add(lotteryNumber);//将彩票号码添加lotteryList中
-			list.remove(roundIndex);//移除刚刚产生的号码
-		}
-		
-		Collections.sort(lotteryList);
-		//对后半段号码排序
-		return lotteryList;
-	}
 	
 	
 	public List<String> getBonus(){
@@ -154,38 +109,7 @@ public class Lottery {
 		return result;
 	}
 	
-	public void generateLottery(String groupNum) {
-		
-		int groupNumber =0;
-		//为了避免不必要的错误，一般在创建变量时都要为其赋初始值
-		
-		groupNumber= Integer.parseInt(groupNum);
-		StringBuilder sbuilder=new StringBuilder();
-		//创建字符串生成器对象，使用字符串生成器能够较为方便地在字符串中追加内容
-		
-		for(int i=0;i<groupNumber;i++) {
-			List<String> startList = getHeadNumber();
-			//获得彩票前半段号码的集合
-			
-			List<String> endList= getBonus();
-			//获得彩票后半段号码的集合
-			
-			for(int m=0;m<startList.size();m++) {
-				sbuilder.append(startList.get(m));
-				//append()即为追加方法；用于添加彩票的前半段号码到字符串生成器中
-				
-			}
-			sbuilder.append("    ");
-			
-			for(int n=0;n<endList.size();n++) {
-				sbuilder.append(endList.get(n));
-				//添加彩票的后半段号码到字符串生成器中
-			}
-			sbuilder.append("\n");
-		}
-		System.out.println(sbuilder.toString());
-		//将每组生成好的彩票号码即时输出
-	}
+	
 	public void output(String groupNum) {
 		int groupNumber =0;
 		//为了避免不必要的错误，一般在创建变量时都要为其赋初始值
