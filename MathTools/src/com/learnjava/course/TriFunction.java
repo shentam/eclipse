65ed61335baecada9369d5cdc711ee1630804dda
Andrew Chao <andrew.m.chao@gmail.com>
2018-03-15 12:37:12 +0000

    this is a math tool example

---
+++ MathTools/src/com/learnjava/course/TriFunction.java
@@ -0,0 +1,16 @@
+package com.learnjava.course;
+
+public class TriFunction {
+
+	public static void main(String[] args) {
+		System.out.println("sin30 = "+Math.sin(Math.PI/6));
+		//计算30°正弦值，参数是用户都表示的角，即π的六分之一
+		System.out.println("cos30 = "+Math.cos(Math.PI/6));
+		//计算30°余弦值，这些计算三角函数的方法，其参数和返回值的类型都为double
+		System.out.println("tan30 = "+Math.tan(Math.PI/6));
+		//计算30°的正切值
+		
+
+	}
+
+}
