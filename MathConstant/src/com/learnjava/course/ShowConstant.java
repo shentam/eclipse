dd7cbd75bb2210598d0cf8a48fac783e9e1b361b
Andrew Chao <andrew.m.chao@gmail.com>
2018-03-15 12:36:36 +0000

    this is a math constant example

---
+++ MathConstant/src/com/learnjava/course/ShowConstant.java
@@ -0,0 +1,11 @@
+package com.learnjava.course;
+
+public class ShowConstant {
+
+	public static void main(String[] args) {
+		System.out.println("The natural logarithm is "+Math.E);
+		System.out.println("The circumference ratio is "+Math.PI);
+
+	}
+
+}
