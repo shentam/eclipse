a69abe7d802da150983afb7387fa7049cbaf2cc9
Andrew Chao <andrew.m.chao@gmail.com>
2018-03-15 09:52:35 +0000

    this is  a Dynamic example which use to create a file object with reflection

---
+++ DynamicClass/src/com/learnjava/course/Test.java
@@ -0,0 +1,28 @@
+package com.learnjava.course;
+
+import java.io.File;
+import java.lang.reflect.Constructor;
+
+public class Test {
+
+	public static void main(String[] args) {
+		try {
+			Constructor<File> constructor= File.class.getDeclaredConstructor(String.class);
+			//获得File类的Constructor对象
+			
+			System.out.println("Create File Object with Reflection.");
+			//使用反射创建File对象
+			
+			File file =constructor.newInstance("C:/Users/潭/Desktop/MyFile.txt");
+			System.out.println("Use File Object to create MyFile.txt on desktop.");
+			//指定了创建的路径为桌面，名称为“MyFile.txt”
+			
+			file.createNewFile();
+			System.out.println("File is created ?" +file.exists());
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+				
+	}
+
+}
